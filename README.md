921263dd334bdf0bfe1c828563f3e8f1b8eebe81
Alicia <chan.aliciaw@gmail.com>
2018-07-20 22:01:23 -0400

    version2- final

--- README.md
+++ README.md
@@ -1,13 +1,2 @@
-# TriviaGame
-
-The Legend of Zelda Trivia Game 
-
-- Added sound 
--added rotating background images 
-
-1. need to decided which image you prefer for "incorrect" answer 
-2. need to confirm all color / styling  
-3. need to confirm if sound is correctly formated 
-4. need to confirm if background images are in correct order 
-5. Do you still want to add Navi gif?  Maybe we can add at end when reporting final tallies ? 
-6. **** Noticed some gramatical errors after just pushing new version, need to double check spelling , grammar **** 
+# Trivia-Game
+Legend of Zelda Trivia Game
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
-	var qaArray, right, wrong, unanswered, currentIndex, timeIsUp;
-
+	var qArray;
+	var correct;
+	var incorrect; 
+	var unanswered; 
+	var currentIndex;
+	var timeIsUp;
+	var audio = new Audio("assets/audio/Tetsugaku - The Legend Of Zelda Ocarina Of Time - Gerudo Valley.mp3");
+	
 	var questionTimer = {
 		time: 15,
 
 		reset: function () {
-        	questionTimer.time = 15;
+			questionTimer.time = 15;
     	},
    		start: function(){
    			$('#time').html("Time Remaining: " + questionTimer.time).css("color", "red");;
@@ -20,85 +26,91 @@ $(document).ready(function() {
 
 	        if(questionTimer.time < 6) {	//At 5 text will start flashing
 	        	if(questionTimer.time % 2 == 0) {
-	        		$('#time').css("color", "green");	//Whiteyellow
+	        		$('#time').css("color", "yellow");
 	        	} else {
-	        		$('#time').css("color", "#E8867D");	//Redpink
+	        		$('#time').css("color", "#90ddff");	//lt blue
 	        	}
 	        }
     	},
 	}
 
 	function varSet() {
-		qaArray = [{
+		qArray = [{
 			question: "Who is the main character of the Legend of Zelda series?",
             answers: ["Biggoron", "Link", "Majora", "Princess Zelda"], 
             imgRight: './assets/images/dancinglink.gif',
-            imgWrong: './assets/images/tenor.gif', 
-            backgrd:'./assets/images/zelda2.jpg',
+            imgWrong: './assets/images/what-link.gif', 
+            backgrd:'./assets/images/zelda-startpage.jpg',
             correctAnswer:1
 		}, {
 			question: "What village is Link from?",
             answers: ["Kokiri Forest", "Gerudo Valley", "Death Mountain", "Hyrule"], 
             imgRight: './assets/images/dancinglink.gif', 
-            imgWrong: './assets/images/tenor.gif', 
-            backgrd:'./assets/images/mountain3.jpg',
+            imgWrong: './assets/images/falling.gif', 
+            backgrd:'./assets/images/bck2.jpg',
             correctAnswer:3
 		}, {
             question: "How many Legend of Zelda games are there?",
             answers: [8, 4, 11, 19], 
             imgRight: './assets/images/dancinglink.gif', 
-            imgWrong: './assets/images/tenor.gif', 
-            backgrd:'./assets/images/zelda2.jpg',
+            imgWrong: './assets/images/cantbelieve.gif', 
+            backgrd:'./assets/images/bck3.jpg',
             correctAnswer:3
 		}, {
 			question: "When did the first game in the series came out?",
             answers: ["February 21, 1986", "July 16, 1969", "November 4, 2008", "November 29, 1972"], 
             imgRight: './assets/images/dancinglink.gif', 
-            imgWrong: './assets/images/tenor.gif',
-            backgrd:'./assets/images/mountain3.jpg', 
+            imgWrong: './assets/images/dying.gif',
+            backgrd:'./assets/images/bck4.jpg', 
             correctAnswer: 0
 		}, {
 			question: "Who is the main villain in the Legend of Zelda series?",
             answers: ["Ghirahim", "Kriemhild", "Shadarastra", "Ganondorf"], 
             imgRight: './assets/images/dancinglink.gif', 
             imgWrong: './assets/images/tenor.gif',
-            backgrd:'./assets/images/zelda2.jpg', 
+            backgrd:'./assets/images/bck5.jpg', 
             correctAnswer: 3
 		}]
 
-		right = 0;
-		wrong = 0;
+		correct = 0;
+		incorrect = 0;
 		unanswered = 0;
 
 		currentIndex = -1;	//Starts at -1 because advance automatically increases it by 1 so it will start at 0
 
-		$('#question').html("<button class='btn' id='start'>Start</button>")
+		$('#question').html("<button class='btn' id='start'>Start</button>");
+		// $('.main-container').css("background-color", "rgba(0, 0, 0, 0)");
 		$('#answer0, #answer1, #answer2, #answer3').hide().off('click');
 
 		$('#start').on("click", function() {
 			advance();
+			audio.play();
+		
 		});
 	}
 
 	function askQuestions() {
 		questionTimer.start();
-		$('#question').html(qaArray[currentIndex].question);
-		$('#answer0').show().html(qaArray[currentIndex].answers[0]);
-		$('#answer1').show().html(qaArray[currentIndex].answers[1]);
-		$('#answer2').show().html(qaArray[currentIndex].answers[2]);
-		$('#answer3').show().html(qaArray[currentIndex].answers[3]);
+		$('.main-container').css("background-color", "rgba(0, 0, 0, .3)");
+		$('#question').html(qArray[currentIndex].question);
+		$('#answer0').show().html(qArray[currentIndex].answers[0]);
+		$('#answer1').show().html(qArray[currentIndex].answers[1]);
+		$('#answer2').show().html(qArray[currentIndex].answers[2]);
+		$('#answer3').show().html(qArray[currentIndex].answers[3]);
 		$('#imgresponse').hide().off('click');
- 
- $('<body>').animate({ opacity: 0}, 1000, function() {
-        $('.background').css("background-image", "url('" + qaArra[currentIndex].backgrd + "')").animate({opacity: 1},1000);
-    });
+		$('body').css("background-image", "url(" + qArray[currentIndex].backgrd + ")");
+		
+		
+		// $('body').css("background-image", "url('" + qaArra[currentIndex].backgrd + "')").animate({opacity: 1},1000);
 		onClickAnswer();
+		audio.play();
+		
 	}
 
 	function onClickAnswer() {
 		$('.btn').on("click", function() {
 			var buttonClick = parseInt($(this).attr('value'));
-			if(buttonClick === qaArray[currentIndex].correctAnswer) {
+			if(buttonClick === qArray[currentIndex].correctAnswer) {
 				rightAnswer();
 			}
 			else {
@@ -109,29 +121,29 @@ $(document).ready(function() {
 
 	function rightAnswer() {
 		clearTimeout(timeIsUp);
-		right++;
+		correct++;
 		questionTimer.stop();
 		questionTimer.reset();
 		$('#time').empty();
-		$('#question').html("<h2>Correct!</h2>");
+		$('#question').html("<h2>You've got it right!</h2>");
 		$('#answer0, #answer1, #answer2, #answer3').hide().off('click');
-		$('#imgresponse').show().html("<img class='gifs' src=" + qaArray[currentIndex].imgRight + ">");
+		$('#imgresponse').show().html("<img class='gifs' src=" + qArray[currentIndex].imgRight + ">");
 
-		timeIsUp = setTimeout(advance, 4 * 1000);
+		timeIsUp = setTimeout(advance, 1* 1000);
 	}
 
 	function wrongAnswer() {
 		clearTimeout(timeIsUp);
-		wrong++;
+		incorrect++;
 		questionTimer.stop();
 		questionTimer.reset();
 		$('#time').empty();
-		$('#question').html("<h2>Nope!</h2>");
+		$('#question').html("<h2>Sorry, you're incorrect!</h2>");
 		$('#answer0, #answer1, #answer2, #answer3').hide().off('click');
-		$('#imgresponse').show().html("The correct answer was: " + qaArray[currentIndex].answers[qaArray[currentIndex].correctAnswer] +
-			"<br><img id='wimg' src=" + qaArray[currentIndex].imgWrong + ">");
+		$('#imgresponse').show().html("The correct answer was: " + qArray[currentIndex].answers[qArray[currentIndex].correctAnswer] +
+			"<br><img id='wimg' src=" + qArray[currentIndex].imgWrong + ">");
 
-		timeIsUp = setTimeout(advance, 4 * 1000);
+		timeIsUp = setTimeout(advance, 1 * 1000);
 	}
 
 	function timesUp() {
@@ -142,17 +154,18 @@ $(document).ready(function() {
 		$('#time').empty();
 		$('#question').html("<h2>Time's Up!</h2>");
 		$('#answer0, #answer1, #answer2, #answer3').hide().off('click');
-		$('#imgresponse').show().html("<p> The correct answer was: </p>" + qaArray[currentIndex].answers[qaArray[currentIndex].correctAnswer] +
-			"<br><img class='gifs' src=" + qaArray[currentIndex].picwrong + ">");
+		$('#imgresponse').show().html("<p> The correct answer was: </p>" + qArray[currentIndex].answers[qArray[currentIndex].correctAnswer] +
+			"<br><img class='gifs' src=" + qArray[currentIndex].imgWrong + ">");
 
-		timeIsUp = setTimeout(advance, 4 * 1000);
+		timeIsUp = setTimeout(advance, 1 * 1000);
 	}
 
 	function endScreen() {
+		audio.pause();
 		$('#time').html("<h2>Good job!</h2>");
-		$('#question').html("Your results <br><br>Right: " + right + "<br>Wrong: " + wrong + "<br>Not Answered: " + unanswered);
+		$('#question').html("Your results: <br><br>Right: " + correct + "<br>Wrong: " + incorrect + "<br>Not Answered: " + unanswered);
 
-		$('#imgresponse').html("<button class='btn' id='playagain'>Play again?</button>")
+		$('#imgresponse').html("<button class='btn' id='playagain'>Play again?</button>");
 
 		$('#playagain').on("click", function() {
 			varSet();
@@ -163,9 +176,9 @@ $(document).ready(function() {
 	function advance() {
 		currentIndex++;
 
-		if(currentIndex < qaArray.length) {
+		if(currentIndex < qArray.length) {
 			askQuestions();
-			timeIsUp = setTimeout(timesUp, 30 * 1000);
+			timeIsUp = setTimeout(timesUp, 15 * 1000);
 		} else {
 			endScreen();
 		}
